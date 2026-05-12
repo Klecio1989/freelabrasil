19e62aa3e3ac197274ed94d04a0283467097d965
Klecio Brito <klecior.brito@hotmail.com>
2026-05-11 22:26:25 -0300

    teste layout minimo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "FreellaBrasil",
@@ -13,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <Navbar />
+      <Footer />
       <body>{children}</body>
     </html>
   );
